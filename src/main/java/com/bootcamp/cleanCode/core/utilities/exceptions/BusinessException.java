bf6727cbf7aecced7f138a0764404d4485403991
Tugay <Tugay@DESKTOP-8EHVPE9>
2025-08-19 17:48:50 +0300

    addedNewExceptions

---
+++ src/main/java/com/bootcamp/cleanCode/core/utilities/exceptions/BusinessException.java
@@ -0,0 +1,9 @@
+package com.bootcamp.cleanCode.core.utilities.exceptions;
+
+public class BusinessException extends RuntimeException {
+    
+    public BusinessException(String message) {
+        super(message);
+    }
+    
+}
--- src/main/java/com/bootcamp/cleanCode/core/utilities/exceptions/RestExceptionHandler.java
+++ src/main/java/com/bootcamp/cleanCode/core/utilities/exceptions/RestExceptionHandler.java
@@ -3,6 +3,8 @@ package com.bootcamp.cleanCode.core.utilities.exceptions;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.bootcamp.cleanCode.core.utilities.ProblemDetails;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,5 +30,21 @@ public class RestExceptionHandler {
         problemDetails.setValidationErrors(errors);
 
         return new ResponseEntity<>(problemDetails, HttpStatus.BAD_REQUEST);
-    }  
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ProblemDetails> handleNotFoundExcepiton(NotFoundException exception){
+        ProblemDetails details = new ProblemDetails();
+        details.setMessage(exception.getMessage());
+
+        return new ResponseEntity<>(details, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ProblemDetails> handleGenericException(Exception exception){
+        ProblemDetails details= new ProblemDetails();
+        details.setMessage("Internal.Server.Error");
+
+        return new ResponseEntity<>(details, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
